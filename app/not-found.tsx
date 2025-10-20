5f2daba013d8fdb90e1acb1fd31ed1321a81a209
Michel <michel.ciardullo@gmail.com>
2025-10-20 03:39:24 +0200

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main className="grid min-h-full place-items-center bg-white dark:bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
@@ -10,12 +12,12 @@ export default function NotFound() {
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            href="/"
             className="rounded-md bg-indigo-600 dark:bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 dark:hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:focus-visible:outline-indigo-500"
           >
             Go back home
-          </a>
+          </Link>
           <a href="#" className="text-sm font-semibold text-gray-900 dark:text-white">
             Contact support <span aria-hidden="true">&rarr;</span>
           </a>
