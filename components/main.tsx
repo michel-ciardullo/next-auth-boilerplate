e1f94144aa2ee2ed530469a5f6c518b74ebfb663
Michel <michel.ciardullo@gmail.com>
2025-10-22 03:13:25 +0200

    Remove section in profile page

--- components/main.tsx
+++ components/main.tsx
@@ -1,11 +1,13 @@
+import { classNames } from "@/utils/functions"
 import { ReactNode } from "react"
 
 interface MainProps {
   children: ReactNode
+  className?: string
 }
 
-export default function Main({ children }: MainProps) {
+export default function Main({ children, className }: MainProps) {
   return (
-    <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{children}</main>
+    <main className={classNames('mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8', className ?? '')}>{children}</main>
   )
 }
